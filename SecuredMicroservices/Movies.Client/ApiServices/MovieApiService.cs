fb861835930e2f28bcca60255486cdd4cfbbb300
Marko Hrncic <marko.hrncic@kogitas.com>
2022-05-30 19:40:01 +0200

    Refactored Movie API service Get Movies method.

--- SecuredMicroservices/Movies.Client/ApiServices/MovieApiService.cs
+++ SecuredMicroservices/Movies.Client/ApiServices/MovieApiService.cs
@@ -10,6 +10,13 @@ namespace Movies.Client.ApiServices
 {
     public class MovieApiService : IMovieApiService
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MovieApiService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public Task<Movie> CreateMovie(Movie movie)
         {
             throw new System.NotImplementedException();
@@ -27,37 +34,15 @@ namespace Movies.Client.ApiServices
 
         public async Task<IEnumerable<Movie>> GetMovies()
         {
-            var apiClientCredentials = new ClientCredentialsTokenRequest
-            {
-                Address = "https://localhost:5005/connect/token",
-                ClientId = "movieClient",
-                ClientSecret = "secret",
-                Scope = "movieAPI"
-            };
+            var httpClient = _httpClientFactory.CreateClient("MovieAPIClient");
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/movies/");
 
-            var client = new HttpClient();
-
-            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5005");
-            if (disco.IsError)
-            {
-                return null;
-            }
-
-            var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
-            if (tokenResponse.IsError)
-            {
-                return null;
-            }
-
-            var apiClient = new HttpClient();
-            apiClient.SetBearerToken(tokenResponse.AccessToken);
-
-            var response = await client.GetAsync("https://localhost:5001/api/movies");
+            var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
             response.EnsureSuccessStatusCode();
+            
 
-            var content = await response.Content.ReadAsStringAsync();
-
-            var movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var movieList = JsonConvert.DeserializeObject<IEnumerable<Movie>>(content);
             return movieList;
         }
 
